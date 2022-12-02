414a60d4359aa5140855fc37cab1826946a1ad1f
Lazy-Bogdi <bmonote@normandiewebschool.fr>
2022-12-02 15:47:52 +0100

    commentaire de lignes

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-const PORT = 8800
+// const PORT = 8800
 const axios = require('axios')
 const cheerio = require('cheerio')
 // const { response } = require('express')
-const express = require('express')
+// const express = require('express')
 
-const app = express()
+// const app = express()
 
-const url = 'http://vps-a47222b1.vps.ovh.net:8484/'
+const url = 'http://vps-a47222b1.vps.ovh.net:8484/product/page/1'
 
+function nextPageOnUrl() {
+    let i = 0
+    let numPage = 1
+    urlTab = []
+    while(i<=7){
+        
+        const url = 'http://vps-a47222b1.vps.ovh.net:8484/product/page/' + numPage
+        urlTab[i] = url
+        numPage++
+        i++
+    }
+    return urlTab
+}
+
+/* while()*/
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
+        // console.log(html)
         const $ = cheerio.load(html)
         const articles = []
 
         $('.card', html).each(function() {
-            const title = $(this).find('.card-body').find('.card-title').text()
-            const img = $(this).find('img').attr('src')
-            const url = $(this).find('a').attr('href')
+            const title = $(this).find('.card-title').text()
+            const imgLink = $(this).find('img').attr('src')
+            const imgClass = $(this).find('img').attr('class')
+            const productPage = $(this).find('a').attr('href')
+            const badgeContent = $(this).find('.badge').text()
+
             articles.push({
                 title,
-                url,
-                img
-
+                productPage,
+                imgLink,
+                imgClass,
+                badgeContent
             })
         })
-        console.log(articles)
+        console.log(articles,nextPageOnUrl())
     }).catch(err => console.log(err))
 
