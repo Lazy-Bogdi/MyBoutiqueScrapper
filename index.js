f04c93d00834f791ff7939ab8f4ce305d7f6e32d
Lazy_Dude <bmonote@normandiewebschool.fr>
2022-12-07 23:30:25 +0100

    changements et test avec fetch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // const PORT = 8800
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
 // const { response } = require('express')
 // const express = require('express')
 
@@ -9,83 +11,66 @@ const cheerio = require('cheerio')
 //const url = 'http://vps-a47222b1.vps.ovh.net:8484/product/page/1'
 
 function nextPageOnUrl() {
-    let i = 0
-    let numPage = 1
-    urlTab = []
+    let i = 0;
+    let numPage = 1;
+    urlTab = [];
     while(i<=7){
         
-        const url = 'http://vps-a47222b1.vps.ovh.net:8484/product/page/' + numPage
-        urlTab[i] = url
-        numPage++
-        i++
+        const url = 'http://vps-a47222b1.vps.ovh.net:8484/product/page/' + numPage;
+        urlTab[i] = url;
+        numPage++;
+        i++;
     }
-    return urlTab
+    return urlTab;
 }
-allLinks = nextPageOnUrl()
+let articles = [];
+const getFakeTee = async (url) => {
+	const response = await fetch(url);
+	const body = await response.text();
+    const $ = cheerio.load(body);
+	//console.log(body); // prints a chock full of HTML richness
+
+    $('.card',body).each(('.card-title') => {
+
+        const title = $(this).find('.card-title').text()
+        const imgLink = $(this).find('img').attr('src')
+        const imgClass = $(this).find('img').attr('class')
+        const productPageUrl = 'http://vps-a47222b1.vps.ovh.net:8484' + $(this).find('a').attr('href') 
+        const badgeContent = $(this).find('.badge').text()
+        
+        articles.push({
+            title,
+            productPageUrl,
+            imgLink,
+            imgClass,
+            badgeContent
+        });
+      });
+	//console.log(articles);
+};
+
+allLinks = nextPageOnUrl();
 //console.log(allLinks)
 
 
-let i = 0
-let articles = []
 
+
+
+
+let i = 0;
 while(i<8) {
-   //console.log(urlTab[i])
-   let url = urlTab[i]
-   axios(url)
-    .then(response => {
-        const html = response.data
-        //let articlesLenghth = 0
-        //console.log(html)
-        const $ = cheerio.load(html)
-        
-
-        $('.card', html).each(function() {
-            const title = $(this).find('.card-title').text()
-            const imgLink = $(this).find('img').attr('src')
-            const imgClass = $(this).find('img').attr('class')
-            const productPage = $(this).find('a').attr('href')
-            const badgeContent = $(this).find('.badge').text()
-
-            articles.push({
-                title,
-                productPage,
-                imgLink,
-                imgClass,
-                badgeContent
-            })
-        })
-        //articlesLenghth = articles.length 
-        //console.log(articlesLenghth)
-        //console.dir(articles, {'maxArrayLength': null})
-        //console.dir(articles)
-    }).catch(err => console.log(err))
-   i++
-   
-}
+    //console.log(urlTab[i])
+    let url = allLinks[i];
+    //console.log(url)
+    let urlReturn =  getFakeTee(url);
+    articles.push({
+        url,
+        urlReturn
+    });
+    i++;
+    console.log(articles)
+ }
 
 
-// axios(url)
-//     .then(response => {
-//         const html = response.data
-//         // console.log(html)
-//         const $ = cheerio.load(html)
-//         const articles = []
 
-//         $('.card', html).each(function() {
-//             const title = $(this).find('.card-title').text()
-//             const imgLink = $(this).find('img').attr('src')
-//             const imgClass = $(this).find('img').attr('class')
-//             const productPage = $(this).find('a').attr('href')
-//             const badgeContent = $(this).find('.badge').text()
-
-//             articles.push({
-//                 title,
-//                 productPage,
-//                 imgLink,
-//                 imgClass,
-//                 badgeContent
-//             })
-//         })
-        
-//     }).catch(err => console.log(err))
 
